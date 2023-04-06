58b02023bba1b485a16b8253d9c869c1dba0a645
ElizaWbr <elizawebernha@gmail.com>
2023-04-06 03:34:56 -0300

    locale switcher

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import i18n from './i18n'
+import router from './router'
+
+import '@/assets/main.css'
 
 loadFonts()
 
 createApp(App)
+  .use(i18n)
+  .use(router)
   .use(vuetify)
   .mount('#app')
