2ac9cb576b24502dfe212e2614a18e11b58d0ea9
ElizaWbr <elizawebernha@gmail.com>
2023-05-01 19:49:36 -0300

    delete old deploy

--- src/components/signin/BackSignInBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="signin__backNavBar">
-        <div class="signin__backNavBar__icon"></div>
-        <div class="signin__backNavBar__icon"></div>
-        <div class="signin__backNavBar__icon"></div>
-    </div>
-</template>
