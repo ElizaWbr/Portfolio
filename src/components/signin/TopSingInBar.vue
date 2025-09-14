7cbc3b06eeddabd3bab63a3d94a1b738bce8ee17
ElizaWbr <elizawebernha@gmail.com>
2025-09-14 17:50:39 -0300

    Updates

--- src/components/signin/TopSingInBar.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="signin__topNavBar">
-        <span class="mdi mdi-window-minimize"></span>
-        <span class="mdi mdi-window-maximize pl-3"></span>
-        <span class="mdi mdi-window-close pl-3"></span>
-    </div>
-</template>
