6d226498a34cba7641cb0264362a8d1c4e98240a
ElizaWbr <elizawebernha@gmail.com>
2025-09-16 01:21:09 -0300

    update myprojects page to get projects and translation from bd

--- src/components/home/MyProjects.vue
+++ src/components/home/MyProjects.vue
@@ -1,113 +1,82 @@
 <template>
-    <div class="d-flex justify-center">
-        <v-card class="myprojects__card">
-            <v-card-title class="page__title pt-4">
-                Series.Show
-            </v-card-title>
-            <v-card class="myprojects__card__link" elevation="8" rounded="lg">
-                <a href="https://elizawbr.github.io/Series.Show/" class="myprojects__card__image">
-                    <v-img cover src="../../../public/img/cover/Series.Show.png" alt="Series.Show home">
-                    </v-img>
-                </a>
+    <v-row class="myprojects__row">
+        <v-col
+        v-for="(project, index) in localizedProjects"
+        :key="index"
+        cols="12" xs="12" sm="6" md="4" lg="3" xl="3"
+        class="myprojects__col"
+        >
+            <v-card class="myprojects__card">
+                <v-card-title class="page__title pt-4">{{ project.title }}</v-card-title>
+                <v-card class="myprojects__card__link" elevation="8" rounded="lg">
+                    <a :href="`${project.url}`" class="myprojects__card__image" target="_blank">
+                        <v-img cover height="15vh" :src="`../../../img/cover/${project.img}`" :alt="`${project.title} home`">
+                        </v-img>
+                    </a>
+                </v-card>
+                <v-card-text class="card__text-justify myprojects__text_card">
+                    <span v-if="project.about">
+                        <v-divider></v-divider>
+                        <br>
+                        <span style="color: #640abd;" class="mdi mdi-circle-medium"></span>
+                        {{ project.about }}
+                    </span>
+                    <span v-if="project.tech">
+                        <br>
+                        <br>
+                        <v-divider></v-divider>
+                        <br>
+                        <span style="color: #640abd;" class="mdi mdi-circle-medium"></span>
+                        {{ project.tech }}
+                    </span>
+                    <span v-if="project.desc">
+                        <br>
+                        <br>
+                        <v-divider></v-divider>
+                        <br>
+                        <span style="color: #640abd;" class="mdi mdi-circle-medium"></span>
+                        {{ project.desc }}
+                    </span>
+                </v-card-text>
             </v-card>
-            <v-card-text class="card__text-justify">
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #640abd;" class="mdi mdi-circle-medium"></span>
-                {{ $t('series.show-about') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #640abd;" class="mdi mdi-circle-medium"></span>
-                {{ $t('series.show-technologies') }}
-            </v-card-text>
-            <v-card-text class="card__text-justify">
-            </v-card-text>
-
-        </v-card>
-        <v-card class="myprojects__card">
-            <v-card-title class="page__title pt-4">
-                {{ $t('tik-tok-project-title') }}
-            </v-card-title>
-            <v-card class="myprojects__card__link" elevation="8" rounded="lg">
-                <a href="https://tiktok---jornadadev-95aa8.web.app" class="myprojects__card__image">
-                    <v-img cover src="../../../public/img/cover/TikTok_Clone.png" alt="TikTok Clone preview">
-                    </v-img>
-                </a>
-            </v-card>
-            <v-card-text class="card__text-justify">
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('tik-tok-project-about') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('tik-tok-project-technologies') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('tik-tok-project-description') }}
-            </v-card-text>
-        </v-card>
-        <v-card class="myprojects__card">
-            <v-card-title class="page__title pt-4">
-                {{ $t('lpiii-project-title') }}
-            </v-card-title>
-            <v-card class="myprojects__card__link" elevation="8" rounded="lg">
-                <a href="https://github.com/ElizaWbr/Trabalho-de-LPII-PHP" class="myprojects__card__image">
-                    <v-img cover src="../../../public/img/cover/PHP_Project.png" alt="Projeto PHP no GitHub">
-                    </v-img>
-                </a>
-            </v-card>
-            <v-card-text class="card__text-justify">
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #ff80b4;" class="mdi mdi-circle-medium"></span>
-                {{ $t('lpiii-project-about') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #ff80b4;" class="mdi mdi-circle-medium"></span>
-                {{ $t('lpiii-project-technologies') }}
-            </v-card-text>
-        </v-card>
-    </div>
-    <div class="d-flex justify-center projects__background">
-        <v-card class="myprojects__card">
-            <v-card-title class="page__title pt-4">
-                {{ $t('portfolio-project-title') }}
-            </v-card-title>
-            <v-card class="myprojects__card__link" elevation="8" rounded="lg">
-                <a href="https://github.com/ElizaWbr/Portfolio" class="myprojects__card__image">
-                    <v-img cover src="../../../public/img/cover/Portfolio-GitHub.png" alt="Projeto PHP no GitHub">
-                    </v-img>
-                </a>
-            </v-card>
-            <v-card-text class="card__text-justify">
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('portfolio-project-about') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('portfolio-project-technologies') }}
-                <br>
-                <br>
-                <v-divider></v-divider>
-                <br>
-                <span style="color: #b548b9;" class="mdi mdi-circle-medium"></span>
-                {{ $t('portfolio-project-description') }}
-            </v-card-text>
-        </v-card>
-    </div>
+        </v-col>
+    </v-row>
 </template>
+
+<script>
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/main.js'
+import i18n from '@/i18n'
+
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    async mounted(){
+        const querySnapshot = await getDocs(collection(db, 'myProjects'));
+        const projects_ = []
+        querySnapshot.forEach((doc) => {
+            projects_.push(doc.data());
+            console.log(projects_);
+        })
+
+        projects_.sort((a, b) => b.priority - a.priority)
+        this.projects = projects_;
+    },
+    computed: {
+        localizedProjects() {
+            return this.projects.map(project => {
+                return {
+                    ...project,
+                    title: project[`title_${i18n.global.locale.value}`] || project[`title_en`],
+                    about: project[`about_${i18n.global.locale.value}`] || project[`about_en`],
+                    tech: project[`tech_${i18n.global.locale.value}`] || project[`tech_en`],
+                    desc: project[`desc_${i18n.global.locale.value}`] || project[`desc_en`],
+                }
+            })
+        }
+    }
+}
+</script>
